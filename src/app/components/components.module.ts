afa2e969eaba636b64b75291a9e020e7bb7226eb
seba_notebook <seba_sanger@hotmail.com>
2021-03-04 03:18:16 -0300

    responsive design of charts pages done

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ChartsModule } from 'ng2-charts';
 import { SalesChartComponent } from './charts/sales-chart/sales-chart.component';
 import { CustomersChartComponent } from './charts/customers-chart/customers-chart.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [SalesChartComponent, CustomersChartComponent],
   exports: [SalesChartComponent, CustomersChartComponent],
-  imports: [CommonModule, ChartsModule],
+  imports: [CommonModule, ChartsModule, MatCardModule, MatGridListModule],
 })
 export class ComponentsModule {}
--- src/app/pages/charts/charts.component.ts
+++ src/app/pages/charts/charts.component.ts
@@ -1,15 +1,27 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.scss']
+  styleUrls: ['./charts.component.scss'],
 })
 export class ChartsComponent implements OnInit {
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  public rows: number;
+  public cols: number;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        this.cols = 2;
+        this.rows = 2;
+      } else {
+        this.cols = 1;
+        this.rows = 1;
+      }
+    })
+  );
 }
