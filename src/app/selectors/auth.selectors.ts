493ecb7cdf7f8860a932207a62b2921b216d1566
seba_notebook <seba_sanger@hotmail.com>
2021-03-20 22:07:20 -0300

    selectors created

--- src/app/selectors/auth.selectors.ts
+++ src/app/selectors/auth.selectors.ts
@@ -1,4 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import * as authReducer from '../reducers/auth.reducer';
 
-export const getUserStatus;
+export const getUserStatus = createFeatureSelector<authReducer.State>('auth');
+
+export const getUser = createSelector(
+  getUserStatus,
+  (state: authReducer.State) => state.user
+);
--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -4,6 +4,9 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { SidebarService } from 'src/app/services/sidebar.service';
 import { AuthService } from 'src/app/auth/auth.service';
+import * as userAuthSelector from '../../selectors/auth.selectors';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-navigation',
@@ -16,15 +19,22 @@ export class NavigationComponent implements OnInit {
   email: string;
   fullName: string;
   avatar: string;
+  user: User;
+
+  user$: Observable<any>;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private authService: AuthService,
-    private sidebarService: SidebarService
+    private sidebarService: SidebarService,
+    private authStore: Store<{ auth: any }>
   ) {
     this.fullName = this.authService.getFullName();
   }
   ngOnInit(): void {
+    this.authStore.select(userAuthSelector.getUser).subscribe((res) => {
+      this.user = res;
+    });
     this.menuItems = this.sidebarService.menu;
     this.authService.loggedIn.subscribe(
       (data: boolean) => (this.isLoggedIn = data)
