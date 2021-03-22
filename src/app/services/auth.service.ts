97ded47b7abc4caaeec2c22230dd94246b3fd451
Seba pc <seba_sanger@hotmail.com>
2021-03-22 00:49:04 -0300

    refresh token deleted TODO in other moment

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,10 @@
-import { Injectable, Output, EventEmitter, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { LoginRequestPayload } from '../interfaces/login-request.payload';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
-import jwt_decode from 'jwt-decode';
 import { GetUserAuthenticated } from '../interfaces/get-user-authenticated';
 import { LoginResponse } from '../interfaces/login-response.payload';
 import { Store } from '@ngrx/store';
@@ -29,36 +28,8 @@ export class AuthService {
     });
   }
 
-  getEmail() {
-    return localStorage.getItem('email');
-  }
-  getRefreshToken() {
-    return localStorage.getItem('refreshToken');
-  }
   getJwtToken(): string {
     return localStorage.getItem('authenticationToken');
-  }
-
-  refreshToken() {
-    const refreshTokenPayload: RefreshTokenPayload = {
-      email: this.getEmail(),
-      refreshToken: this.getRefreshToken(),
-    };
-    return this.httpClient
-      .post<LoginResponse>(
-        'http://localhost:8080/auth/refresh/token',
-        refreshTokenPayload
-      )
-      .pipe(
-        tap((data: any) => {
-          this.authStore.dispatch(SET_USER({ user: new User(data.user) }));
-          this.setUserDataOnStorageAndRemoveOld(data);
-        }),
-        catchError((err) => {
-          this.logout();
-          return throwError(err);
-        })
-      );
   }
 
   login(loginRequestPayload: LoginRequestPayload): Observable<boolean> {
@@ -76,15 +47,11 @@ export class AuthService {
   setUserDataOnStorageAndRemoveOld(data: LoginResponse) {
     this.removeDataFromStorage();
     localStorage.setItem('authenticationToken', data.authenticationToken);
-    localStorage.setItem('email', data.user.email);
-    localStorage.setItem('refreshToken', data.refreshToken);
     localStorage.setItem('expiresAt', data.expiresAt.toString());
   }
 
   removeDataFromStorage() {
     localStorage.removeItem('authenticationToken');
-    localStorage.removeItem('refreshToken');
-    localStorage.removeItem('email');
     localStorage.removeItem('expiresAt');
   }
 
